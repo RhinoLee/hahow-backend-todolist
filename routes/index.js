a04d3bb4e0d55ee19ab1341bb9ecae173292c05a
kewang <cpckewang@gmail.com>
2021-07-30 06:58:22 +0800

    Complete create item

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
 var express = require("express");
 var router = express.Router();
 
+const LIST = {};
+
 router.get("/", (req, res, next) => {
-  res.render("index");
+  return res.render("index", {
+    LIST,
+  });
 });
 
 router.post("/items", (req, res, next) => {
-  console.log(req.body);
+  if (!LIST[req.body.itemDate]) {
+    // initial date
+    LIST[req.body.itemDate] = {
+      doing: [],
+    };
+  }
+
+  LIST[req.body.itemDate].doing.push({
+    text: req.body.itemText,
+  });
+
+  return res.redirect("/");
 });
 
 module.exports = router;
