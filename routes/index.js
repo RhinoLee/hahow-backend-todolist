b18f4b34309f5d5eafd6e4c8dc0cd90e74a60044
kewang <cpckewang@gmail.com>
2021-07-29 23:15:58 +0800

    Add form to add item

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.post("/items", (req, res, next) => {
+  console.log(req.body);
 });
 
 module.exports = router;
