b40a359fa1bf5e026ca04902773b4cfc4dbe4a73
kewang <cpckewang@gmail.com>
2021-07-29 22:58:37 +0800

    Add user check

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", {
-    user: {
-      name: "kewang",
-    },
-  });
+  res.render("index");
 });
 
 module.exports = router;
