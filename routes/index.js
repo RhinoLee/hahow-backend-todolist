e0426f52d84c64501eef94d1efc7834385cefacf
kewang <cpckewang@gmail.com>
2021-08-01 16:01:52 +0800

    Add create item

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+const LIST = {};
+
 router.get("/", function (req, res, next) {
-  res.render("index", {
+  console.dir(LIST);
+
+  return res.render("index", {
     user: {
-      name: "kewang"
-    }
+      name: "kewang",
+    },
+    LIST,
   });
+});
+
+router.post("/items", (req, res, next) => {
+  const { itemDate, itemText } = req.body;
+
+  if (!LIST[itemDate]) {
+    // initial
+    LIST[itemDate] = {
+      doing: [],
+    };
+  }
+
+  LIST[itemDate].doing.push({
+    text: itemText,
+  });
+
+  return res.redirect("/");
 });
 
 module.exports = router;
