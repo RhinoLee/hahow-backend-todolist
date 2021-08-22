bb96299641f63ebe38c4dbc262dccac08970554b
kewang <cpckewang@gmail.com>
2021-08-22 12:37:34 +0800

    Add MongoDB support

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,44 @@
 var express = require("express");
 var uuidv4 = require("uuid").v4;
+var dotenv = require("dotenv");
+var { MongoClient } = require("mongodb");
+
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_DATABASE_NAME = process.env.MONGODB_DATABASE_NAME;
+
+const client = new MongoClient(MONGODB_URL);
 
 var router = express.Router();
 
 const LIST = {};
 
-router.get("/", function (req, res, next) {
-  console.dir(LIST);
+const getDb = async () => {
+  await client.connect();
+
+  return client.db(MONGODB_DATABASE_NAME);
+};
+
+router.get("/", async (req, res, next) => {
+  const db = await getDb();
+
+  const days = await db
+    .collection("TodoList")
+    .find()
+    .sort({
+      _id: -1,
+    })
+    .toArray();
+
+  const LIST = {};
+
+  days.forEach((day) => {
+    LIST[day._id] = {
+      doing: day.doing,
+      done: day.done,
+    };
+  });
 
   return res.render("index", {
     user: {
@@ -16,48 +48,108 @@ router.get("/", function (req, res, next) {
   });
 });
 
-router.post("/items", (req, res, next) => {
+router.post("/items", async (req, res, next) => {
   const { itemDate, itemText } = req.body;
 
-  if (!LIST[itemDate]) {
-    // initial
-    LIST[itemDate] = {
-      doing: [],
-      done: [],
-    };
-  }
+  const db = await getDb();
 
-  LIST[itemDate].doing.push({
-    id: uuidv4(),
-    text: itemText,
+  const day = await db.collection("TodoList").findOne({
+    _id: itemDate,
   });
 
+  const item = {
+    id: uuidv4(),
+    text: itemText,
+  };
+
+  if (day) {
+    await db.collection("TodoList").updateOne(
+      {
+        _id: itemDate,
+      },
+      {
+        $push: {
+          doing: item,
+        },
+      }
+    );
+  } else {
+    await db.collection("TodoList").insertOne({
+      _id: itemDate,
+      doing: [item],
+      done: [],
+    });
+  }
+
   return res.redirect("/");
 });
 
-router.post("/items/delete", (req, res, next) => {
-  const idx = LIST[req.body.itemDate].doing.findIndex(
-    (item) => item.id === req.body.itemId
+router.post("/items/delete", async (req, res, next) => {
+  const { itemDate, itemId, itemState } = req.body;
+
+  const db = await getDb();
+
+  await db.collection("TodoList").updateOne(
+    {
+      _id: itemDate,
+    },
+    {
+      $pull: {
+        [itemState]: {
+          id: itemId,
+        },
+      },
+    }
   );
 
-  LIST[req.body.itemDate].doing.splice(idx, 1);
-
   return res.redirect("/");
 });
 
-router.put("/items/state", (req, res, next) => {
+router.put("/items/state", async (req, res, next) => {
   const { itemId, itemDate, itemState } = req.body;
 
-  const idx = LIST[itemDate][itemState].findIndex((item) => item.id === itemId);
+  const db = await getDb();
 
-  if (idx === -1) {
+  const day = await db.collection("TodoList").findOne({
+    _id: itemDate,
+    [itemState]: {
+      $elemMatch: {
+        id: itemId,
+      },
+    },
+  });
+
+  if (!day) {
     return res.fail();
   }
 
-  const item = LIST[itemDate][itemState].splice(idx, 1)[0];
+  await db.collection("TodoList").updateOne(
+    {
+      _id: itemDate,
+    },
+    {
+      $pull: {
+        [itemState]: {
+          id: itemId,
+        },
+      },
+    }
+  );
+
+  const item = day[itemState][0];
+
   const toState = itemState === "doing" ? "done" : "doing";
 
-  LIST[itemDate][toState].push(item);
+  await db.collection("TodoList").updateOne(
+    {
+      _id: itemDate,
+    },
+    {
+      $push: {
+        [toState]: item,
+      },
+    }
+  );
 
   return res.success({
     item,
