d288c84e3e400422e28c4db4a7b9a81dc651b8cc
Rhino Lee <picklerhino@gmail.com>
2021-09-26 20:48:44 +0800

    change right block HTML to PUG

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { 
+    user: {
+      name: "Rhino"
+    },
+    todos: [
+      {
+        title: "瑜珈一小時"
+      },
+      {
+        title: "想一個SideProject"
+      },
+      {
+        title: "線上課程看一單元"
+      },
+    ],
+    doneTodos: [
+      {
+        title: "背10個單字"
+      },
+      {
+        title: "整理書櫃"
+      },
+      {
+        title: "寫一篇文章"
+      },
+    ],
+  });
 });
+
+
 
 module.exports = router;
